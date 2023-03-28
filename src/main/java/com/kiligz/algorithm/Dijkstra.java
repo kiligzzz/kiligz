eaab9d9edb37a9aba261f5380d0dd3d8f1cb03f7
zhuyifan10045 <zyfkilig@gmail.com>
2023-03-28 18:07:19 +0800

    Dijkstra最短路径算法

--- src/main/java/com/kiligz/algorithm/Dijkstra.java
+++ src/main/java/com/kiligz/algorithm/Dijkstra.java
@@ -3,56 +3,75 @@ package com.kiligz.algorithm;
 import java.util.*;
 
 /**
- * Dijkstra算法
+ * Dijkstra算法（基于边）
  * 求最短路径
  *
  * @author Ivan
  * @since 2023/3/27
  */
-public class Dijkstra<T> {
+public class Dijkstra {
     /**
      * 默认权重
      */
-    public static final int DEFAULT_WEIGHT = 10;
+    public static final int DEFAULT_WEIGHT = 1;
 
     /**
      * 图，节点 -> （目标节点 -> 到目标节点的权重）
      */
-    private final Map<T, Map<T, Integer>> graph;
-
-    public Dijkstra() {
-        this.graph = new HashMap<>();
-    }
-
-    public Dijkstra(Map<T, Map<T, Integer>> graph) {
-        this.graph = graph;
-    }
+    private final Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
 
     /**
      * 添加边
      */
-    public void addEdge(T source, T dest, int weight) {
+    public void addEdge(int source, int dest, int weight) {
         graph.computeIfAbsent(source, k -> new HashMap<>())
                 .put(dest, weight);
     }
 
     /**
+     * 删除边
+     */
+    public void removeEdge(int source, int dest, int weight) {
+        graph.get(source).remove(dest, weight);
+    }
+
+    /**
+     * 1. 图加入特定边（peekSource,peekDest,weight）
+     * 2. 求节点（source）到节点（dest）的最短路径
+     * 3. 将图恢复原样
+     */
+    public int peekShortest(int source, int dest, int peekSource, int peekDest, int weight) {
+        addEdge(peekSource, peekDest, weight);
+        Integer shortest = shortestPath(source, dest);
+        removeEdge(peekSource, peekDest, weight);
+        return shortest;
+    }
+
+    public static Dijkstra drawGraph(int edgeCount) {
+        Dijkstra dijkstra = new Dijkstra();
+        for (int i = 0; i < edgeCount; i++) {
+            dijkstra.addEdge(i, i + 1, DEFAULT_WEIGHT);
+        }
+        return dijkstra;
+    }
+
+    /**
      * 到指定目标节点的最短路径
      */
-    public Integer shortestPath(T source, T destination) {
-        return shortestPath(source).get(destination);
+    public Integer shortestPath(Integer source, Integer dest) {
+        return shortestPath(source).get(dest);
     }
 
     /**
      * 所有目标节点的最短路径
      */
-    public Map<T, Integer> shortestPath(T source) {
+    public Map<Integer, Integer> shortestPath(Integer source) {
         // 目标节点 -> 源节点到目标节点的距离
-        Map<T, Integer> destToDistanceMap = new HashMap<>();
+        Map<Integer, Integer> destToDistanceMap = new HashMap<>();
         // 已经遍历过的节点
-        Set<T> visited = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
         // 目标节点优先队列，按源节点到目标点的距离从小到大排
-        PriorityQueue<T> queue = new PriorityQueue<>(Comparator.comparingInt(destToDistanceMap::get));
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(destToDistanceMap::get));
 
         // 初始化
         destToDistanceMap.put(source, 0);
@@ -60,14 +79,15 @@ public class Dijkstra<T> {
 
         // 循环直到graph遍历完
         while (!queue.isEmpty()) {
-            T cur = queue.poll();
+            Integer cur = queue.poll();
             if (!visited.add(cur)) continue;
 
             // 目标节点 -> 当前节点到目标节点的权重
-            Map<T, Integer> destToWeightMap = graph.getOrDefault(cur, new HashMap<>());
+            Map<Integer, Integer> destToWeightMap = graph.getOrDefault(cur, new HashMap<>());
             destToWeightMap.forEach((dest, weight) -> {
                 // 当前节点到新目标节点的距离
                 int newDistance = destToDistanceMap.get(cur) + weight;
+
                 // 记录最短路径
                 Integer oldDistance = destToDistanceMap.get(dest);
                 if (oldDistance == null || oldDistance > newDistance) {
@@ -76,6 +96,7 @@ public class Dijkstra<T> {
                 }
             });
         }
+
         return destToDistanceMap;
     }
 
