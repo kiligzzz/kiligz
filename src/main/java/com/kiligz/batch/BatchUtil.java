93a17d23ca90b55c28dad10ac2f87d3988e57fc5
zhuyifan <zhuyifan1@popmart.com>
2026-02-25 11:35:53 +0800

    feat: 新增分成指定批次

--- src/main/java/com/kiligz/batch/BatchUtil.java
+++ src/main/java/com/kiligz/batch/BatchUtil.java
@@ -19,11 +19,19 @@ public class BatchUtil {
     /**
      * 按指定大小分批
      */
-    public static <T> List<List<T>> batching(Collection<T> c, int batchSize) {
+    public static <T> List<List<T>> bySize(Collection<T> c, int batchSize) {
         List<T> list = new ArrayList<>(c);
         return Stream.iterate(0, i -> i + batchSize)
                 .limit((list.size() + batchSize - 1) / batchSize)
                 .map(i -> list.subList(i, Math.min(i + batchSize, list.size())))
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * 分成指定批次
+     */
+    public static <T> List<List<T>> byCount(Collection<T> c, int batchCount) {
+        int batchSize = (int) Math.ceil((double) c.size() / batchCount);
+        return bySize(c, batchSize);
     }
 }
